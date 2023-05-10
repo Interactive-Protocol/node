f7852e337e0c36c1281c910f88e01d8530672227
dfam13 <133124260+dfam13@users.noreply.github.com>
2023-05-10 21:52:56 +0200

    Add ZKP calls

--- node.js
+++ node.js
@@ -1,5 +1,7 @@
 const Web3 = require("web3");
 const contractABI = require("./contractABI.json");
+const snarkjs = require("snarkjs");
+const { circuit, witness } = require("./zkpCircuit");
 
 // Set up the Web3 instance
 const web3 = new Web3("http://localhost:8545"); // Replace with your Ethereum provider URL
@@ -8,8 +10,11 @@ const web3 = new Web3("http://localhost:8545"); // Replace with your Ethereum pr
 const contractAddress = "0x1234567890abcdef"; // Replace with your contract address
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
+// Define the proving and verification keys
+const { provingKey, verificationKey } = snarkjs["groth"].setup(circuit);
+
 // Listen for new task events
-contract.events.NewTask({}, (error, event) => {
+contract.events.NewTask({}, async (error, event) => {
   if (error) {
     console.error("Error in event listener:", error);
     return;
@@ -18,9 +23,14 @@ contract.events.NewTask({}, (error, event) => {
   const { taskId, taskData } = event.returnValues;
   console.log("New task received:", taskId, taskData);
 
-  // Perform bid computation and submit bid
-  const bid = computeBid(taskData); // Custom bid computation logic
-  submitBid(taskId, bid);
+  // Perform ZKP and bid computation
+  const isProofValid = await performZKP(taskData);
+  if (isProofValid) {
+    const bid = computeBid(taskData); // Custom bid computation logic
+    submitBid(taskId, bid);
+  } else {
+    console.log("ZKP proof is not valid. Bid not submitted.");
+  }
 });
 
 // Submit a bid for a task
@@ -39,6 +49,27 @@ function submitBid(taskId, bid) {
     .on("error", (error) => {
       console.error("Error submitting bid:", error);
     });
+}
+
+// Perform zero-knowledge proof (ZKP)
+async function performZKP(taskData) {
+  // Convert task data to the format expected by the circuit
+  const inputData = Buffer.from(taskData);
+
+  try {
+    // Generate the proof
+    const proof = await snarkjs["groth"].prove(provingKey, witness, inputData);
+
+    // Verify the proof
+    const isValid = snarkjs["groth"].verify(verificationKey, proof);
+
+    console.log("Proof is valid:", isValid);
+
+    return isValid;
+  } catch (error) {
+    console.error("Error performing ZKP:", error);
+    return false;
+  }
 }
 
 // Compute the bid for a given task data (Custom logic)
@@ -67,6 +98,9 @@ async function startNode() {
     console.error("Error starting the node:", error);
   }
 }
+
+// Start the node
+
 
 // Start the node
 startNode();
