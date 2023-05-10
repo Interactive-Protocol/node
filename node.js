6c4fd627ee7d2d9a13e84ba7af38776117f661ba
dfam13 <133124260+dfam13@users.noreply.github.com>
2023-05-10 15:36:58 +0200

    Update bid mechanism

--- node.js
+++ node.js
@@ -1,57 +1,72 @@
-const Web3 = require('web3');
-const contractAbi = require('./InteractiveProtocolContractAbi.json');
+const Web3 = require("web3");
+const contractABI = require("./contractABI.json");
 
-// Connect to the Ethereum network
-const web3 = new Web3('http://localhost:8545'); // Replace with your Ethereum node URL
-const contractAddress = '0x123456789ABCDEF'; // Replace with the actual contract address
+// Set up the Web3 instance
+const web3 = new Web3("http://localhost:8545"); // Replace with your Ethereum provider URL
 
-// Create an instance of the smart contract
-const contract = new web3.eth.Contract(contractAbi, contractAddress);
+// Load the contract ABI and address
+const contractAddress = "0x1234567890abcdef"; // Replace with your contract address
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-// Subscribe to events emitted by the contract
-contract.events.TaskSubmitted((error, event) => {
+// Listen for new task events
+contract.events.NewTask({}, (error, event) => {
   if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('New task submitted:', event.returnValues.taskId);
+    console.error("Error in event listener:", error);
+    return;
   }
+
+  const { taskId, taskData } = event.returnValues;
+  console.log("New task received:", taskId, taskData);
+
+  // Perform bid computation and submit bid
+  const bid = computeBid(taskData); // Custom bid computation logic
+  submitBid(taskId, bid);
 });
 
-// Listen for new tasks and bid on them
-contract.events.NewTask((error, event) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const { taskId, taskData } = event.returnValues;
-    console.log('New task:', taskId);
-    
-    // Perform computation and determine the bid
-    const bid = computeBid(taskData);
-    
-    // Submit the bid to the contract
-    contract.methods.submitBid(taskId, bid)
-      .send({ from: '0xYourNodeAddress', gas: 200000 })
-      .then(() => {
-        console.log('Bid submitted successfully.');
-      })
-      .catch((error) => {
-        console.error('Error submitting bid:', error);
-      });
-  }
-});
+// Submit a bid for a task
+function submitBid(taskId, bid) {
+  const accounts = web3.eth.getAccounts();
+  const account = accounts[0]; // Replace with the desired Ethereum account
 
-// Start listening for events
-contract.events.allEvents({}, (error, event) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('Event:', event.event);
-  }
-});
-
-// Function to perform computation and determine the bid
-function computeBid(taskData) {
-  // Your custom logic to compute the bid based on the task data
-  // ...
-  return 100; // Replace with the actual bid amount
+  contract.methods.submitBid(taskId, bid)
+    .send({ from: account })
+    .on("transactionHash", (hash) => {
+      console.log("Bid submitted. Transaction hash:", hash);
+    })
+    .on("receipt", (receipt) => {
+      console.log("Bid receipt:", receipt);
+    })
+    .on("error", (error) => {
+      console.error("Error submitting bid:", error);
+    });
 }
+
+// Compute the bid for a given task data (Custom logic)
+function computeBid(taskData) {
+  // Example custom logic: Compute the bid as a percentage of the task data length
+  const bidPercentage = 0.05; // 5% of task data length
+  const taskDataLength = taskData.length;
+  const bid = taskDataLength * bidPercentage;
+
+  return bid;
+}
+
+// Other functions for interacting with the contract (e.g., getting task details, submitting results, etc.) can be added here
+
+// Main entry point
+async function startNode() {
+  try {
+    // Connect to the Ethereum network
+    await web3.eth.net.isListening();
+
+    console.log("Node started. Listening for new tasks...");
+
+    // Other initialization logic or background processes can be added here
+
+  } catch (error) {
+    console.error("Error starting the node:", error);
+  }
+}
+
+// Start the node
+startNode();
